c57f30e2c24765e50c61d00a738b3627674a78a1
Justin Turner <jmturner638@gmail.com>
2021-07-30 06:48:25 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    start_num = int(input())
+
+    while start_num <= 100:
+        print(start_num)
+        start_num += 1
 
 if __name__ == '__main__':
     main()
